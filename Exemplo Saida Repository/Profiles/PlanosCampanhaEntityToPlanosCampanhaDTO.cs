ec1de76178356a824018b35f37e7173cb218ec70
Rodrigo Machado <rodrigo.machado@topdown.com.br>
2021-05-03 14:15:33 -0300

    Adicionando arquivo de profile

---
+++ Exemplo Saida Repository/Profiles/PlanosCampanhaEntityToPlanosCampanhaDTO.cs
@@ -0,0 +1,13 @@
+﻿using TopDown.Comercial.Repository.Domain.Core.Entities.Campanha;
+using TopDown.Comercial.Repository.DTO.Campanha;
+
+namespace TopDown.Comercial.Repository.Infra.CrossCutting.IoC.Profile
+{
+    internal class PlanosCampanhaEntityToPlanosCampanhaDTO : AutoMapper.Profile
+    {
+        public PlanosCampanhaEntityToPlanosCampanhaDTO()
+        {
+            CreateMap<PlanosEntity, PlanosCampanhaDTO>().ReverseMap();
+        }
+    }
+}
